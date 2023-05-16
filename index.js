b13bc16106f94707ea475357309af657b11c8f39
Kartick Jana <kartickjana020@gmail.com>
2023-05-16 16:56:03 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg">
+    <h1 className="heading">Super Over League</h1>
+    <div className="container">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      >
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
